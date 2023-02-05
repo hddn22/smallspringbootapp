2e5748fbc0af152e25b21b04d8863ccd0e0ad779
hidden <hiddenwebuser@gmail.com>
2023-02-06 01:50:39 +0300

    Create project

---
+++ src/main/java/com/example/smallspringbootapp/SmallspringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallspringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallspringbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smallspringbootapp/SmallspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
